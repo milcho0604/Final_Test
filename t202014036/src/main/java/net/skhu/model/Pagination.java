fd447ce76a5d516ecb46dd95cb330e12c298b498
milcho0604 <milcho0604@gmail.com>
2023-07-06 15:54:27 +0900

    model의 title을 공백으로 선언하여 검색의 조건 오류를 수정.

--- t202014036/src/main/java/net/skhu/model/Pagination.java
+++ t202014036/src/main/java/net/skhu/model/Pagination.java
@@ -8,7 +8,7 @@ public class Pagination {
     int sz = 10;       // 페이지 당 레코드 수
     int di = 0;        // departmentId
     int recordCount;   // 전체 레코드 수
-    String title = " "; // 검색할 타이틀 선언
+    String title; // 검색할 타이틀 선언
 
     public String getQueryString() {
         return String.format("pg=%d&sz=%d&di=%d", pg, sz, di);
